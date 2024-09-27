67c1324c2c6fbc6fd0a7752844c1eb4b65c3f1c7
songmin0111 <violet2111@naver.com>
2024-09-27 23:25:00 +0900

    board 패키지 생성

---
+++ 3주차 과제/myTemplateCode/src/test/java/com/pironeer/mytemplatecode/MyTemplateCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pironeer.mytemplatecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTemplateCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
